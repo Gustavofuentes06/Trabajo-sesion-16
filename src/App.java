8a5401bdce48b2e326fb82504b499b6e631a3fd0
Gustavofuentes06 <gustavorafaelfuentescorea@gmail.com>
2024-04-26 18:09:03 -0600

    version 2

--- src/App.java
+++ src/App.java
@@ -5,11 +5,13 @@ public class App {
         public static int num1;
     public static int num2;
     public static int num3;
+    // suma de numeros pequeños 
     public static int sumarpequeños (int num1,int num2, int num3){
         int menor= Math.min(num1, num2) ;
         int menor2= Math.min(Math.max(num1, num2), num3) ;
         return menor2+menor; 
     }
+    //Resta de numeros grandes 
     public static int restagrandes (int num1, int num2, int num3){
         int mayor= Math.max(num1, num2);
         int mayor2= Math.max(Math.min(num1, num2), num3); 
@@ -19,6 +21,7 @@ public class App {
             return mayor-mayor2; 
         }
     }
+    // Encontrar el numero mayor 
     public static int numeromayor (int num1, int num2, int num3) {
         int mayor2= Math.max(Math.max(num1, num2), num3); 
         return mayor2; 
@@ -40,6 +43,8 @@ public class App {
 
     } while (num1 == num2 || num1 == num3 || num2 == num3);
 
+    // Se muestran las operaciones ya efectuadas 
+    
     System.out.println("La suma de los números más pequeños es "+sumarpequeños(num1, num2, num3));
     System.out.println("La resta de los números más grandes es  "+restagrandes(num1, num2, num3));
     System.out.println("El número mayor es "+numeromayor(num1, num2, num3)); 
